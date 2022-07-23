a4bc0444ebec23bde3e598edd81a8907e2f3016c
Nathan <nathanlemosv@gmail.com>
2022-07-23 13:24:56 -0300

    Initial commit

---
+++ test/serverless-opensearch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessOpensearch from '../lib/serverless-opensearch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-opensearch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessOpensearch.ServerlessOpensearchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
