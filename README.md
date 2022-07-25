8b30379f51e761f4abccdfac8b47fadc21024638
Nathan <nathanlemosv@gmail.com>
2022-07-25 12:39:33 -0300

    added lambda into stack

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# Welcome to your CDK TypeScript project
+# IAC - Serverless to Opensearch
 
-This is a blank project for CDK development with TypeScript.
+This is a simple CDK project that implements an AWS SQS topic to recieve documents and an AWS Lambda subscrivied to the topic that send de document to an opensearch index definied in the project.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
+The project needs a env-var called `AWS_ACCOUNT_ID` with the AWS account id as value.
 
-## Useful commands
+## To run
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
+* `[Install AWS CDK](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html)`
 * `cdk synth`       emits the synthesized CloudFormation template
+* `cdk bootstrap`   set resources to create de stack in AWS
+* `cdk deploy --all --require-approval never`      deploy this stack to your default AWS account/region
+
+
--- lib/serverless-opensearch-stack.ts
+++ lib/serverless-opensearch-stack.ts
@@ -1,16 +1,67 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Aws, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AccountPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { Domain, EngineVersion } from 'aws-cdk-lib/aws-opensearchservice';
 
 export class ServerlessOpensearchStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { domainEndpoint, masterUserPassword } = new Domain(this, 'opensearch', {
+      version: EngineVersion.OPENSEARCH_1_2,
+      accessPolicies: [
+        new PolicyStatement({
+          actions: ['es:ESHttpGet', 'es:ESHttpPost', 'es:ESHttpPut'],
+          effect: Effect.ALLOW,
+          principals: [new AccountPrincipal(props?.env?.account)],
+          resources: ['*'],
+        }),
+      ],
+      capacity: {
+        masterNodes: 2,
+        masterNodeInstanceType: 't3.small.search',
+        dataNodes: 1,
+        dataNodeInstanceType: 't3.small.search'
+      },
+      fineGrainedAccessControl: {
+        masterUserName: 'master'
+      },
+      logging: {
+        slowSearchLogEnabled: true,
+        appLogEnabled: true,
+        slowIndexLogEnabled: true,
+      },
+      useUnsignedBasicAuth: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessOpensearchQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topic = new Topic(this, 'document-saved-topic', {
+      topicName: `document-saved`,
+      displayName: `document-saved`
+    });
+    
+    topic.addToResourcePolicy(new PolicyStatement({
+      actions: ['SNS:GetTopicAttributes', 'SNS:SetTopicAttributes', 'SNS:AddPermission',
+        'SNS:RemovePermission', 'SNS:DeleteTopic', 'SNS:Subscribe',
+        'SNS:ListSubscriptionsByTopic', 'SNS:Publish', 'SNS:Receive'],
+      resources: [topic.topicArn],
+      principals: [new AccountPrincipal(Aws.ACCOUNT_ID)]
+    }));
+
+    const lambda = new Function(this, 'document-saved-function', {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset('resources'),
+      handler: 'document-saved.main',
+      timeout: Duration.seconds(5),
+      environment: {
+        OPENSEARCH_ENDPOINT: domainEndpoint,
+        OPENSEARCH_SECRET: masterUserPassword?.unsafeUnwrap() || ''
+      }
+    });
+
+    topic.addSubscription(new LambdaSubscription(lambda));
+
   }
 }
